42d3e54b4baa405453c428497fd39e481c6f1eb6
amitksms97 <amitksms97@gmail.com>
2021-05-01 12:04:45 +0530

    Add project files.

---
+++ LearningDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, string> config = new Dictionary<string, string>();
+
+            config.Add("resolution", "1930*1080");
+            config.Add("title", "MyWebsite"); 
+
+            Console.WriteLine(config["title"]);
+
+            foreach(var setting in config)
+            {
+                Console.WriteLine(setting.Value);
+            }
+        }
+    }
+}
